3a1eebf14f6b6751041d914c6477b8990c2f82b6
jeauxrdhy <jordhytapia@outlook.com>
2023-06-02 22:03:39 -0700

    added preliminary authentication to form

--- contactopedia/src/Components/ContactIndex/ContactIndex.jsx
+++ contactopedia/src/Components/ContactIndex/ContactIndex.jsx
@@ -50,18 +50,41 @@ class ContactIndex extends React.Component
 
     handleAddContact = (newContact) =>
     {
-        const newFinalContact = 
-        {   
-            ...newContact,
-            id:this.state.contactList[this.state.contactList.length-1].id+1,
-            isFavorite: false
+        if(newContact.name == '')
+        {
+            return{status: 'failure', msg: 'Please enter a valid name'}
         }
-        this.setState((prevState)=>{
-            return{
-                contactList: prevState.contactList.concat([newFinalContact])
+        else if(newContact.phone == '')
+        {
+            return{status: 'failure', msg: 'Please enter a valid phone number'}
+        }
+
+        const duplicateRecord = this.state.contactList.filter((x)=>{
+            if(x.name == newContact.name && x.phone == newContact.phone)
+            {
+                return true
             }
         })
-        alert('hello')
+
+        if(duplicateRecord.length > 0)
+        {
+            return{status: 'failure', msg: 'Duplicate Record'}
+        }
+        else
+        {
+            const newFinalContact = 
+            {   
+                ...newContact,
+                id:this.state.contactList[this.state.contactList.length-1].id+1,
+                isFavorite: false
+            }
+            this.setState((prevState)=>{
+                return{
+                    contactList: prevState.contactList.concat([newFinalContact])
+                }
+            })
+            return{status: 'success', msg: 'Contact was added successfully'}
+        }
     }
 
     render()
