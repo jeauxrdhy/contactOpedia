4f5416a61177a2d751144a8a4fe17873d7ce38aa
jeauxrdhy <jordhytapia@outlook.com>
2023-06-03 05:07:17 -0700

    Add random contact functionality added

--- contactopedia/src/Components/AddRandomContact/AddRandomContact.jsx
+++ contactopedia/src/Components/AddRandomContact/AddRandomContact.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { getRandomUser } from '../../Utilities/Api'
 
-function AddRandomContact() {
+const GetRandomContact = async (props) => {
+  const responseFromAPI = await getRandomUser()
+  console.log(responseFromAPI)
+
+  return props.handleAddRandomContact({
+    name: responseFromAPI.data.first_name + " " + responseFromAPI.data.last_name,
+    email: responseFromAPI.data.email,
+    phone: responseFromAPI.data.phone_number 
+
+  })
+}
+
+
+function AddRandomContact(props) {
   return (
     <div>
-        <button className="btn btn-danger form-control">
+        <button 
+        className="btn btn-success form-control"
+        onClick={()=>GetRandomContact(props)}>
             Add Random Contact
         </button>
     </div>
--- contactopedia/src/Components/ContactIndex/ContactIndex.jsx
+++ contactopedia/src/Components/ContactIndex/ContactIndex.jsx
@@ -111,6 +111,19 @@ class ContactIndex extends React.Component
         })
     }
 
+    handleAddRandomContact = (newContact) => {
+        const newFinalContact = {   
+            ...newContact,
+            id:this.state.contactList[this.state.contactList.length-1].id+1,
+            isFavorite: false
+        }
+        this.setState((prevState)=>{
+            return{
+                contactList: prevState.contactList.concat([newFinalContact])
+            }
+        })
+    }
+
 
 
     render()
@@ -120,8 +133,9 @@ class ContactIndex extends React.Component
                 <Header />
                 <div className="container" style={{minHeight:'85vh'}}>
                     <div className="row py-3">
-                        <div className="col-4 offset-2">
-                            <AddRandomContact />
+                        <div className="col-4 offset-2 row">
+                            <AddRandomContact
+                            handleAddRandomContact = {this.handleAddRandomContact} />
                         </div>
                         <div className="col-4">
                             <RemoveAllContacts />
